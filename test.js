fdffea9b9911ac1c14bc2915a73ed6689ce53e02
simonepri <simone.pri@hotmail.it>
2017-07-21 17:45:21 +0200

    Implement basic tests

--- test.js
+++ test.js
@@ -2,5 +2,11 @@ import test from 'ava';
 import m from '.';
 
 test('empty test', t => {
-	t.is(m, m);
+  const obj = {};
+  process.env.TEST_ONE = 5;
+
+  m.extend(obj, 'TEST');
+  console.log(obj);
+  t.is(obj.one, 5);
+  delete process.env.TEST_ONE;
 });
