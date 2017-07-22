4fde45c10a0851120a6ac180fb1c97913ba65d10
simonepri <simone.pri@hotmail.it>
2017-07-22 09:56:30 +0200

    Improve tests

--- test.js
+++ test.js
@@ -1,12 +1,19 @@
 import test from 'ava';
 import m from '.';
 
-test('empty test', t => {
-  const obj = {};
+test('Should return an object for nested envs', t => {
   process.env.TEST_ONE = 5;
 
-  m(obj, 'test');
-  t.is(obj.one, 5);
+  t.is(typeof m.get('test'), 'object');
+  t.is(m.get('test').one, 5);
+
+  delete process.env.TEST_ONE;
+});
+
+test('Should return a value for full env path', t => {
+  process.env.TEST_ONE = 5;
+
+  t.is(m.get('test.one'), 5);
 
   delete process.env.TEST_ONE;
 });
