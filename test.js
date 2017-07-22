9b6b9b930fa2c6306407d99dfe18ca2c39835399
chqma <chq.matteo@gmail.com>
2017-07-22 17:32:18 +0200

    Add test

--- test.js
+++ test.js
@@ -17,3 +17,29 @@ test('Should return a value for full env path', t => {
 
   delete process.env.TEST_ONE;
 });
+
+test('Default value', t => {
+  t.is(m.get('a.b', 0), 0);
+});
+
+test('Set environment variable', t => {
+  m.set('a.b', 0);
+  t.is(process.env.A_B, '0');
+  delete process.env.A_B;
+});
+
+test('Check whether environment variable exists', t => {
+  t.is(m.has('a.b', 0), false);
+});
+
+test('Delete environment variable exists', t => {
+  process.env.A_B = 0;
+  m.delete('a.b');
+  t.is(process.env.A_B, undefined);
+});
+
+test('Transform fails', t => {
+  process.env['..'] = 'ciao';
+  t.is(m.get('..'), undefined);
+  delete process.env['..'];
+});
